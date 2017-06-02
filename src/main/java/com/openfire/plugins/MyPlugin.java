f7ef2b5bb9fbf3ea99871299bbb875cf039c3a3e
jwerba <julian.werba@gmail.com>
2017-06-01 22:24:03 -0300

    Initial commit of the template project

---
+++ src/main/java/com/openfire/plugins/MyPlugin.java
@@ -0,0 +1,22 @@
+package com.openfire.plugins;
+
+import java.io.File;
+
+import org.jivesoftware.openfire.container.Plugin;
+import org.jivesoftware.openfire.container.PluginManager;
+
+
+public class MyPlugin implements Plugin {
+
+	public void destroyPlugin() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void initializePlugin(PluginManager arg0, File arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
